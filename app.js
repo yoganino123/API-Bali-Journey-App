6e6a915827123860ffbb386a2814c0c912622f3e
yoganino123 <yoganinorahardian@gmail.comm>
2022-10-11 09:18:30 +0700

    Swagger API Docs

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const app = express();
 const port = process.env.PORT;
 const routes = require("./routes");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(routes);
 app.use("/images", express.static("images"));
